d1344beef27268b529c210332e6c32665b9a5618
salonwaledev <salonwale.dev@gmail.com>
2026-05-08 17:51:17 +0530

    pushed to play store

--- lib/app/config/network/api_constants.dart
+++ lib/app/config/network/api_constants.dart
@@ -1,5 +1,9 @@
 class ApiConstants {
-  static const String baseUrl = 'https://iddev.citronsoftwares.com';
+
+  /// Testing
+  static const String baseUrl = 'https://testidapp.citronsoftwares.com';
+  /// Production
+  // static const String baseUrl = 'https://iddev.citronsoftwares.com';
   static const String login = '/api/auth/login';
   static const String getSchoolUser = '/api/idrecords/getschoolforuser';
   static const String schoolIdList = '/api/IDRecords/filter';
